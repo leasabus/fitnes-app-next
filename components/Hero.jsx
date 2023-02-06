2cdc4cdf67d49a3881b0be97de9c7763266adf40
Leandro Sabus <leandro.sabus@gmail.com>
2023-02-06 11:27:54 -0300

    hero responsive

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Hero = () => {
     return (
         <>
-            <div className='grid grid-cols-2 p-2 mt-4 h-[90vh]'>
+            <div className='flex flex-col md:grid md:grid-cols-2 p-2 mt-4 h-[90vh]'>
 
                 <div className='flex flex-col items-center text-center justify-center p-4 gap-2'>
                     <h2 className='text-2xl text-textRed font-bold'>Fitness club</h2>
@@ -13,7 +13,7 @@ export const Hero = () => {
                     <button className='bg-textRed text-font mt-2  rounded p-1'>Explore Exercises</button>
                 </div>
 
-                <div className='absolute top-0 right-0 w-[50%] h-full p-4' >
+                <div className='w-full md:absolute md:top-0 md:right-0 md:w-[50%] h-full p-4' >
                     <img src="/assets/images/banner.png" alt="#" />
                 </div>
 
--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 export const NavBar = () => {
     return (
-        <div className='flex flex-row items-center justify-between w-[50%] p-4'>
-            <div className='flex items-start justify-start '>
+        <div className='flex flex-row items-center   justify-between w-auto md:w-[50%] p-4'>
+            <div className='flex items-center  md:items-start md:justify-start '>
                 <img src="/assets/images/Logo.png" width={32} alt="" />
             </div>
             <div className='text-textBlack relative flex flex-row items-center justify-center p-4 gap-4'>
